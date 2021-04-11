2b73420abf203b44aa4697277cde67b37afe91db
Fabian Cook <hello@fabiancook.dev>
2021-04-11 22:47:04 +1200

    Allow children to generate multiple times

--- src/children.ts
+++ src/children.ts
@@ -6,27 +6,34 @@ import { Native } from "./native";
 import { withOptions } from "./with-options";
 import { Input } from "@opennetwork/progressive-merge/dist/async";
 
-export async function *children(node: VNode): AsyncIterable<ElementDOMNativeVNode[]> {
-  if (!node.children) return;
-  for await (const children of node.children) {
-    if (!children.length) {
-      continue;
+export function children(node: VNode): AsyncIterable<ElementDOMNativeVNode[]> {
+  return {
+    async *[Symbol.asyncIterator]() {
+      yield *childrenGenerator();
     }
-    if (children.every(isElementDOMNativeVNode)) {
-      yield [...children];
-      continue;
-    }
-    // We have a bunch of iterables, async or not, that will provide an array of
-    // ElementDOMNativeVNode for each iteration
-    const lanes: LaneInput<ElementDOMNativeVNode[]> = children
-      .map(withOptions({}, Native))
-      .map(elementChildren);
-    const merged: AsyncIterable<ReadonlyArray<ElementDOMNativeVNode[] | undefined>> = merge(lanes);
-    for await (const parts of merged) {
-      yield parts.reduce<ElementDOMNativeVNode[]>(
-        (updates , part) => updates.concat(part ?? []),
-        []
-      );
+  };
+  async function *childrenGenerator(): AsyncIterable<ElementDOMNativeVNode[]> {
+    if (!node.children) return;
+    for await (const children of node.children) {
+      if (!children.length) {
+        continue;
+      }
+      if (children.every(isElementDOMNativeVNode)) {
+        yield [...children];
+        continue;
+      }
+      // We have a bunch of iterables, async or not, that will provide an array of
+      // ElementDOMNativeVNode for each iteration
+      const lanes: LaneInput<ElementDOMNativeVNode[]> = children
+        .map(withOptions({}, Native))
+        .map(elementChildren);
+      const merged: AsyncIterable<ReadonlyArray<ElementDOMNativeVNode[] | undefined>> = merge(lanes);
+      for await (const parts of merged) {
+        yield parts.reduce<ElementDOMNativeVNode[]>(
+          (updates , part) => updates.concat(part ?? []),
+          []
+        );
+      }
     }
   }
 
