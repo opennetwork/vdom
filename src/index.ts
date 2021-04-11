a58c47ade762473282937811e0fc29bd4094b7b0
Fabian Cook <hello@fabiancook.dev>
2021-04-11 22:16:26 +1200

    Export ElementDOMNativeVNode and FragmentDOMNative types

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from "./jsx";
 export * from "./context";
 export * from "./document-node";
 export * from "./attributes";
+export type { ElementDOMNativeVNode } from "./element";
+export type { FragmentDOMNative } from "./fragment";
